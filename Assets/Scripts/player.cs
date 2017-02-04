b1ba8620a20075507281cb1f090c84e0a51e4ae0
skuli <skuliarnas@gmail.com>
2017-02-04 13:01:20 +0000

    added movement to player

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
+	float speed = 5f;
+	float rotateSpeed = 3f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,14 @@ public class player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//gameObject.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * speed * Time.deltaTime);
+		gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
+
+		if(Input.GetKey(KeyCode.A)){
+			gameObject.transform.Rotate(new Vector3 (0, -rotateSpeed, 0));
+		}
+		if(Input.GetKey(KeyCode.D)){
+			gameObject.transform.Rotate(new Vector3 (0, rotateSpeed, 0));
+		}
 	}
 }
