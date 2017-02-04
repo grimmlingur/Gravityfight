cac2a878f871dfd3b83212cfb5b16b8ee90ae95e
berglindoma13 <berglindoma13@ru.is>
2017-02-04 16:12:43 +0000

    corner boundary movement fixed

--- Assets/Scripts/BoundaryMove.cs
+++ Assets/Scripts/BoundaryMove.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoundaryMove : MonoBehaviour {
-	private float xBoundary = 11;
-	private float yBoundary = 5;
+	private float xBoundary = 35;
+	private float yBoundary = 20;
+
+	private float offsetX = 34;
+	private float offsetY = 19;
+
 	void OnTriggerExit(Collider other){
 		if ((other.transform.position.x < -xBoundary) || (other.transform.position.x > xBoundary)) {
-			other.transform.position = new Vector3 (-other.transform.position.x, other.transform.position.y, other.transform.position.z); 
+			if ((other.transform.position.y < -offsetY) || (other.transform.position.y > offsetY)) {
+				other.transform.position = new Vector3 (-other.transform.position.x, -other.transform.position.y, other.transform.position.z); 
+			} else {
+				other.transform.position = new Vector3 (-other.transform.position.x, other.transform.position.y, other.transform.position.z); 
+			}
 		} else if ((other.transform.position.y < -yBoundary) || (other.transform.position.y > yBoundary)) {
-			other.transform.position = new Vector3 (other.transform.position.x, -other.transform.position.y, other.transform.position.z); 
-
+			if ((other.transform.position.x < -offsetX) || (other.transform.position.x > offsetX)) {
+				other.transform.position = new Vector3 (-other.transform.position.x, -other.transform.position.y, other.transform.position.z);
+			} else {
+				other.transform.position = new Vector3 (other.transform.position.x, -other.transform.position.y, other.transform.position.z); 
+			}
 		}
 	}
 }
