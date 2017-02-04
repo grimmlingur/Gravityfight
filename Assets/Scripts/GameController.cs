5433b3c642c4706a18944e85f02e846f2e7d7c4b
skuli <skuliarnas@gmail.com>
2017-02-04 14:03:26 +0000

    gamecontroller added and control scheme

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+
+	int player1Lives = 3;
+	int player2Lives = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -6,22 +6,32 @@ public class player : MonoBehaviour {
 
 	float speed = 5f;
 	float rotateSpeed = 3f;
+	int controls;
 
 	// Use this for initialization
 	void Start () {
-		
+		//Gets the controlScheme for the player. GetControlScheme returns either 0 or 1 based on where the 
+		//player is in the gameworld. If 0 then the player uses asd to control and if 1 then he uses jkl.
+		controls = GetControlScheme.getControls (gameObject.transform.position.x);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//gameObject.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * speed * Time.deltaTime);
 		gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
-
-		if(Input.GetKey(KeyCode.A)){
-			gameObject.transform.Rotate(new Vector3 (0, -rotateSpeed, 0));
-		}
-		if(Input.GetKey(KeyCode.D)){
-			gameObject.transform.Rotate(new Vector3 (0, rotateSpeed, 0));
+		if (controls == 0) {
+			if (Input.GetKey (KeyCode.A)) {
+				gameObject.transform.Rotate (new Vector3 (0, -rotateSpeed, 0));
+			}
+			if (Input.GetKey (KeyCode.D)) {
+				gameObject.transform.Rotate (new Vector3 (0, rotateSpeed, 0));
+			}
+		} else {
+			if (Input.GetKey (KeyCode.J)) {
+				gameObject.transform.Rotate (new Vector3 (0, -rotateSpeed, 0));
+			}
+			if (Input.GetKey (KeyCode.L)) {
+				gameObject.transform.Rotate (new Vector3 (0, rotateSpeed, 0));
+			}
 		}
 	}
 }
