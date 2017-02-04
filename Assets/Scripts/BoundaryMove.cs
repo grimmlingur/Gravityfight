b04e9d657f3ff51ee40ff350ddab97f8f1665f82
berglindoma13 <berglindoma13@ru.is>
2017-02-04 15:23:11 +0000

    main scene boundary and skybox added

---
+++ Assets/Scripts/BoundaryMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryMove : MonoBehaviour {
+	private float xBoundary = 11;
+	private float yBoundary = 5;
+	void OnTriggerExit(Collider other){
+		if ((other.transform.position.x < -xBoundary) || (other.transform.position.x > xBoundary)) {
+			other.transform.position = new Vector3 (-other.transform.position.x, other.transform.position.y, other.transform.position.z); 
+		} else if ((other.transform.position.y < -yBoundary) || (other.transform.position.y > yBoundary)) {
+			other.transform.position = new Vector3 (other.transform.position.x, -other.transform.position.y, other.transform.position.z); 
+
+		}
+	}
+}
