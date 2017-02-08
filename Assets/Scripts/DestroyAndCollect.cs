ea50a26781d45e746a8e9700e2a3c774d0279ed8
skuli <skuliarnas@gmail.com>
2017-02-08 11:06:03 +0000

    added collisions and ui

--- Assets/Scripts/DestroyAndCollect.cs
+++ Assets/Scripts/DestroyAndCollect.cs
@@ -8,6 +8,18 @@ public class DestroyAndCollect : MonoBehaviour {
 		if (other.tag == "Boundary") {
 			return;
 		}
+		//If asteroid is uncollected
+
+		if (gameObject.tag == "Player1") {
+			if (other.gameObject.tag == "Player2") {
+				FindObjectOfType<GameController> ().takeDamage (other.gameObject.tag);
+			}
+		} else if (gameObject.tag == "Player2") {
+			if (other.gameObject.tag == "Player1") {
+				FindObjectOfType<GameController> ().takeDamage (other.gameObject.tag);
+			}
+		}
+		//Should asteroids destroy each other?
 		Destroy (gameObject);
 	}
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameController : MonoBehaviour {
 
@@ -10,17 +12,38 @@ public class GameController : MonoBehaviour {
 	int player1Lives = 3;
 	int player2Lives = 3;
 
+	GameObject[] UIObjects;
+	GameObject[] EndUIObjects;
+
 	// Use this for initialization
 	void Start () {
+		UIObjects   = GameObject.FindGameObjectsWithTag("UI");
+		EndUIObjects    = GameObject.FindGameObjectsWithTag("EndUI");
 
+		foreach(GameObject i in EndUIObjects){
+			i.SetActive(false);
+		}
+		foreach(GameObject i in UIObjects){
+			if (i.name == "Player1Lives") {
+				i.gameObject.GetComponent<Text> ().text = "Player 1 Lives: " + player1Lives;
+			}else if (i.name == "Player2Lives") {
+				i.gameObject.GetComponent<Text> ().text = "Player 2 Lives: " + player2Lives;
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		foreach(GameObject i in UIObjects){
+			if (i.name == "Player1Lives") {
+				i.gameObject.GetComponent<Text> ().text = "Player 1 Lives: " + player1Lives;
+			}else if (i.name == "Player2Lives") {
+				i.gameObject.GetComponent<Text> ().text = "Player 2 Lives: " + player2Lives;
+			}
+		}
 	}
 
-	void takeDamage(string tag){
+	public void takeDamage(string tag){
 		if (tag == "Player1") {
 			player1Lives--;
 			livesCheck (tag);
@@ -34,6 +57,7 @@ public class GameController : MonoBehaviour {
 		if (tag == "Player1") {
 			if (player1Lives == 0) {
 				Debug.Log ("Player 1 loses!!!!");
+				displayWin (2);
 			} else {
 				player1.GetComponentInChildren<player> ().respawn ();
 
@@ -41,8 +65,22 @@ public class GameController : MonoBehaviour {
 		} else {
 			if (player2Lives == 0) {
 				Debug.Log ("Player 2 loses!!!!");
+				displayWin (1);
 			} else {
 				player2.GetComponentInChildren<player> ().respawn ();
+			}
+		}
+	}
+
+	void displayWin(int winner){
+		foreach(GameObject i in EndUIObjects){
+			i.SetActive(true);
+			if (i.name == "WinText") {
+				if (winner == 1) {
+					i.gameObject.GetComponent<Text> ().text = "Player 1 Wins!";
+				} else {
+					i.gameObject.GetComponent<Text> ().text = "Player 2 Wins!";
+				}
 			}
 		}
 	}
